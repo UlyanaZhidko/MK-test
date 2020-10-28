1724b18f0ccdd793cfe90f4b02144c539a041b43
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-10-28 22:31:05 +0300

    28.10

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
-$(function(){
+$(function () {
 
     $('.photo-slider').slick({
-        
-        prevArrow:'<button type="button" class="slick-prev"><img src="img/left.svg" alt=""></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="img/right.svg" alt=""></button>',
 
-        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
+
+        autoplay: true,
         autoplaySpeed: 2000,
     });
 
-    $('.menu-btn').on('click', function() {
-        $('.menu-list').slideToggle();
+    $('.main-principles-photo-slider').slick({
+
+        prevArrow: '<button type="button" class="slider-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
+        nextArrow: '<button type="button" class="slider-next"><img src="img/right.svg" alt="arrow: next"></button>',
+
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
+
+    $('.reviews-cards-slider').slick({
+
+        prevArrow: '<button type="button" class="reviews-slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
+        nextArrow: '<button type="button" class="reviews-slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
+        dotsClass: 'slick-dots',
+
+        infinite: true,
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    });
+
+
+
+
 });
