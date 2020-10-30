c63a8111bb4dcf25ed67df86089b3b67568ebe40
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-10-30 21:22:30 +0300

    30.10

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 $(function () {
 
-    $('.photo-slider').slick({
+  $('.photo-slider').slick({
 
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
 
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-    $('.main-principles-photo-slider').slick({
+  $('.main-principles-photo-slider').slick({
 
-        prevArrow: '<button type="button" class="slider-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
-        nextArrow: '<button type="button" class="slider-next"><img src="img/right.svg" alt="arrow: next"></button>',
+    prevArrow: '<button type="button" class="slider-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
+    nextArrow: '<button type="button" class="slider-next"><img src="img/right.svg" alt="arrow: next"></button>',
 
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-    $('.reviews-cards-slider').slick({
+  $('.reviews-cards-slider').slick({
 
-        prevArrow: '<button type="button" class="reviews-slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
-        nextArrow: '<button type="button" class="reviews-slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
-        dotsClass: 'slick-dots',
+    prevArrow: '<button type="button" class="reviews-slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
+    nextArrow: '<button type="button" class="reviews-slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
+    dotsClass: 'slick-dots',
 
-        infinite: true,
-        dots: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-    });
+    infinite: true,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 });
 
 
