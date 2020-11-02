cd030dce6f7ff9cd933036a37c0f9d596bd705ee
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-11-02 13:30:06 +0300

    test

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function () {
     prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="arrow: next"></button>',
 
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
   });
 
@@ -14,7 +14,7 @@ $(function () {
     prevArrow: '<button type="button" class="slider-prev"><img src="img/left.svg" alt="arrow: prev"></button>',
     nextArrow: '<button type="button" class="slider-next"><img src="img/right.svg" alt="arrow: next"></button>',
 
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
   });
 
@@ -41,52 +41,39 @@ $(function () {
   });
 
 
-
-
-  $('.menu-btn').on('click', function () {
-    $('.header-menu').toggle();
-  });
-
-
   /* скрывать офис в минске */
   $('.footer-item-title-minsk').on('click', function () {
     $('.footer-item-list-minsk, .footer-adress-minsk, .footer-phone-minsk').toggle();
   });
-  /* скрывать офис в минске */
+  /* скрывать офис в могилеве */
   $('.footer-item-title-mogilev').on('click', function () {
     $('.footer-adress-mogilev').toggle();
   });
-  /* скрывать офис в минске */
+  /* скрывать офис в бресте */
   $('.footer-item-title-brest').on('click', function () {
     $('.footer-adress-brest').toggle();
   });
-  /* скрывать офис в минске */
+  /* скрывать офис в витебстке */
   $('.footer-item-title-vitebsk').on('click', function () {
     $('.footer-adress-vitebsk').toggle();
   });
-  /* скрывать офис в минске */
+  /* скрывать офис в гродно */
   $('.footer-item-title-grodno').on('click', function () {
     $('.footer-adress-grodno').toggle();
   });
- /* скрывать офис в минске */
- $('.footer-item-title-gomel').on('click', function () {
-  $('.footer-adress-gomel').toggle();
-});
-
-
-
-
-
-
-
-});
-
-
-
-//маска для ввода номера телефона
-var elements = document.getElementsByClassName('cooperation-form-input');
-for (var i = 0; i < elements.length; i++) {
-  new IMask(elements[i], {
-    mask: '+{375}(00)000-00-00',
+  /* скрывать офис в гомеле */
+  $('.footer-item-title-gomel').on('click', function () {
+    $('.footer-adress-gomel').toggle();
   });
-}
+
+
+
+  //маска для ввода номера телефона
+  var elements = document.getElementsByClassName('cooperation-form-input');
+  for (var i = 0; i < elements.length; i++) {
+    new IMask(elements[i], {
+      mask: '+{375}(00)000-00-00',
+    });
+  }
+
+});
